1006417bfe4dd84e0a6ed3035e2701abe4a03702
Oz Guevara <oaarias@utp.edu.co>
2020-08-24 18:43:56 -0500

    create directory

---
+++ learn/hi.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct T {int a; float b;};
+
+int main() {
+    int a = 3;
+
+    string s = "mundirijillo";
+    vector<int> v = {1,2,3,4,5};
+    vector<T> v2 = {{5,1.2}, {2, 5.6}};
+    cout << "Hello word 1" << endl;
+    cout << "Hello word 2" << endl;
+    cout << "Hello word 3" << endl;
+    cout << "Hello word 4" << endl;
+    cout << "Hello word 5" << endl;
+} 
